13376f4cbdb87a0436547463cd7d1635d5a4ac33
ZetaxFTW <ernzerhofffinn@gmail.com>
2022-12-30 18:35:47 +0100

    Created project

--- src/main/java/dev/zetax/IsInsideRegion.java
+++ src/main/java/dev/zetax/IsInsideRegion.java
@@ -5,7 +5,11 @@ import me.TechsCode.UltraCustomizer.base.item.XMaterial;
 import me.TechsCode.UltraCustomizer.scriptSystem.objects.*;
 import me.TechsCode.UltraCustomizer.scriptSystem.objects.datatypes.DataType;
 import net.raidstone.wgevents.WorldGuardEvents;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.io.File;
 
 
 public class IsInsideRegion extends Element {
@@ -53,13 +57,21 @@ public class IsInsideRegion extends Element {
         final Player player = (Player)getArguments(elementInfo)[0].getValue(scriptInstance);
         final String region = (String)getArguments(elementInfo)[1].getValue(scriptInstance);
 
-        final Boolean result = WorldGuardEvents.isPlayerInAnyRegion(player.getUniqueId(), region);
-        System.out.println("Result: " + result);
-        getOutcomingVariables(elementInfo)[0].register(scriptInstance, new DataRequester() {
-            public Object request() {
-                return result;
+        try {
+            File f = new File("plugins/WorldGuardEvents-1.18.1.jar");
+            if(f.exists() && !f.isDirectory()) {
+                getOutcomingVariables(elementInfo)[0].register(scriptInstance, new DataRequester() {
+                    public Object request() {
+                        return WorldGuardEvents.isPlayerInAnyRegion(player.getUniqueId(), region);
+                    }
+                });
+            } else {
+                Bukkit.getServer().getOnlinePlayers().stream().filter(player1 -> player1.isOp()).forEach(player1 -> player1.playSound(player1.getLocation(), "block.note_block.bass", 1, 1));
+                Bukkit.getServer().getOnlinePlayers().stream().filter(player1 -> player1.isOp()).forEach(player1 -> player1.sendTitle(ChatColor.RED + "WorldGuardEvents is", ChatColor.RED + "not installed", 10, 70, 20));
             }
-        });
+        } catch (Exception e) {
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error: " + e.getMessage());
+        }
         getConnectors(elementInfo)[0].run(scriptInstance);
     }
 }
